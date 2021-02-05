73264d7489579494b65d9556be00cab26c9a608c
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-02-05 12:41:29 +0530

    lazy loading routes

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -19,7 +19,7 @@ export class AuthGuard implements CanActivate {
   ): boolean | Observable<boolean> | Promise<boolean> {
     const isAuth = this.authService.getISAuth();
     if(!isAuth) {
-      this.router.navigate(['/login']);
+      this.router.navigate(['auth/login']);
     }
     return isAuth;
   }
